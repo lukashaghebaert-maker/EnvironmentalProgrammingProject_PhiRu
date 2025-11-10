7f6b3ac867549e4d6df3e81385960663a7c52af7
Nadeen Espejo <knaespejo@gmail.com>
2025-11-10 14:03:34 +0100

    Update opendata.py

--- Data/opendata.py
+++ Data/opendata.py
@@ -4,6 +4,22 @@ Created on Mon Nov 10 13:47:07 2025
 
 @author: kristine espejo
 """
+import sqlite3
 
-f = open("impactdb.v1.0.2.dg_filled.db", 'r')
-data=f.readlines()
+#giving the file
+db_file = "impactdb.v1.0.2.dg_filled.db"  # por ejemplo "data.db"
+
+#connecting to the database
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+
+#listing of all tables in the database
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = cursor.fetchall()
+
+print("tables found in the database:")
+for t in tables:
+    print("-", t[0])
+
+#closing the connection
+conn.close()
