6fc0177e0ddf1d3e60b23623d1b40b66a03ade39
Nadeen Espejo <knaespejo@gmail.com>
2025-11-10 14:43:29 +0100

    Created Environment(yml) and updated opendata.py

--- Data/opendata.py
+++ Data/opendata.py
@@ -2,24 +2,35 @@
 """
 Created on Mon Nov 10 13:47:07 2025
 
-@author: kristine espejo
+@author: PhiRu
 """
 import sqlite3
 
-#giving the file
+
+#putting the name of the file in one variable
 db_file = "impactdb.v1.0.2.dg_filled.db"  # por ejemplo "data.db"
 
 #connecting to the database
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-#listing of all tables in the database
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
+#cursor is showing/listing all tables in the database
+#sqlite_master is an internal table that stores the structure of the database; 
+#cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+#tables = cursor.fetchall()
 
-print("tables found in the database:")
-for t in tables:
-    print("-", t[0])
+import panda as pd
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+all_tables = [t[0] for t in cursor.fetchall()]
+print("All tables in DB:", all_tables)
+
+
+total_tables = [t for t in all_tables if t.startswith("Total")]
+
+# Por simplicidad, si hay más de una tabla Total, podemos concatenarlas
+total_tables = [t for t in all_tables if t.startswith("Total")]
+L1 = pd.concat([pd.read_sql_query(f"SELECT * FROM {t}", conn) for t in total_tables], ignore_index=True)
+print("L1 DataFrame loaded. Shape:",L1.shape)
 
 #closing the connection
 conn.close()
